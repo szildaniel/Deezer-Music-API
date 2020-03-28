6f41c8f88867b2facf3fc44653998a63917ee810
Daniel Mydlarz <szildaniel@gmail.com>
2020-03-28 12:00:15 +0100

    add rAF polyfill to index.js trying to fix  smoothscroll behavior on iOS devices

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -1629,14 +1629,13 @@ var _searchCore = require("./search/searchCore");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+_smoothscrollPolyfill.default.polyfill();
+
 function initApp() {
   (0, _render.default)();
   (0, _setData.setData)();
   (0, _enjoyBtnAnimations.enjoyBtnHoverAnimation)();
   (0, _nav.toggleNav)();
-
-  _smoothscrollPolyfill.default.polyfill();
-
   (0, _hamburgerAnimation.hamburgerAnimation)();
   (0, _setResponsiveNavHeight.setResponsiveNavHeight)();
   (0, _setCurrentClassToNavLinks.setCurrentClassToNavLinks)();
@@ -1645,7 +1644,34 @@ function initApp() {
   (0, _searchCore.listenForSearch)();
 }
 
-initApp();
+initApp(); //raf polyfill
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// MIT license
+
+(function () {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) window.requestAnimationFrame = function (callback, element) {
+    var currTime = new Date().getTime();
+    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    var id = window.setTimeout(function () {
+      callback(currTime + timeToCall);
+    }, timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+  };
+  if (!window.cancelAnimationFrame) window.cancelAnimationFrame = function (id) {
+    clearTimeout(id);
+  };
+})();
 },{"../styles/style.scss":"styles/style.scss","./render/render":"js/render/render.js","./setData":"js/setData.js","./helpers/nav":"js/helpers/nav.js","smoothscroll-polyfill":"../node_modules/smoothscroll-polyfill/dist/smoothscroll.js","./animations/hamburgerAnimation":"js/animations/hamburgerAnimation.js","./animations/enjoyBtnAnimations":"js/animations/enjoyBtnAnimations.js","./helpers/setResponsiveNavHeight":"js/helpers/setResponsiveNavHeight.js","./helpers/setCurrentClassToNavLinks":"js/helpers/setCurrentClassToNavLinks.js","./playerActions/listenForPlay":"js/playerActions/listenForPlay.js","./animations/listenForHover":"js/animations/listenForHover.js","./search/searchCore":"js/search/searchCore.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -1674,7 +1700,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56691" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49227" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
